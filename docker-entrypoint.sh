b0139d622882bcdb1911da8340fcc6dc32e0a592
Glenn Brown <glenn.brown@gmail.com>
2024-07-11 14:59:41 -0400

    Further tweaks for SSL certs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-if ! [ -r /etc/ssl/certs/soju/$(hostname -f).crt ] || ! [ -r /etc/ssl/certs/soju/$(hostname -f).key ]
+if ! [ -r /etc/ssl/certs/$(hostname -f).crt ] || ! [ -r /etc/ssl/certs/$(hostname -f).key ]
 then
     echo Cannot find Soju certificate and/or key. 1>&2
     ls -la /etc/ssl/certs/soju 1>&2
@@ -14,6 +14,7 @@ then
     echo "No existing configuration creating a default configuration file!"
     cp /etc/soju/config.default /etc/soju/config
     echo hostname $(hostname -f) >> /etc/soju/config
+    echo tls /etc/ssl/certs/$(hostname -f).crt /etc/ssl/certs/$(hostname -f).key >> /etc/soju/config
 fi
 
 exec su - -c 'exec soju --config /etc/soju/config' soju
