0623c488c35bd4c43d6d98df353bb0203fea552f
Glenn Brown <glenn.brown@gmail.com>
2024-07-13 22:01:44 -0400

    Getting close, some more tweaks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -eux
 
-if ! [ -r /etc/ssl/certs/soju/soju.crt ] || ! [ -r /etc/ssl/certs/soju/soju.key ]
-then
-    echo Cannot find Soju certificate and/or key. 1>&2
-    ls -la /etc/ssl/certs/soju 1>&2
-    exit 1
-fi
-
-cp /etc/soju/config.no-hostname /etc/soju/config
-echo hostname $(hostname --fqdn) >> /etc/soju/config
+mkdir -p /var/lib/soju/logs /etc/ssl/certs/soju /home/soju/uploads /etc/soju /run/soju
+chown -R soju:soju /home/soju/uploads /run/soju /var/lib/soju /etc/soju/config
 exec su - -c 'exec soju -config /etc/soju/config' soju
